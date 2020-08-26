39d40f8df8fb0775c152f3164af8aada23f21ece
Ana <ana.eng.dev@gmail.com>
2020-08-26 13:00:49 -0300

    select the time of duration sound

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ const app = () => {
   const sounds = document.querySelectorAll('.sound-picker button');
   //time display
   const timeDisplay = document.querySelector('.time-display')
-  //get delete de outline
+  const timeSelect = document.querySelectorAll('.time-select button')
+  //get the length de outline
   const outlineLength = outline.getTotalLength()
   // duration
   let fakeDuration = 600
@@ -18,6 +19,14 @@ const app = () => {
   // play sound
   play.addEventListener('click', () => {
     checkPlaying(song)
+  })
+
+  //Select Sound
+  timeSelect.forEach(option => {
+    option.addEventListener('click', function () {
+      fakeDuration = this.getAttribute('data-time')
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}: ${Math.floor(fakeDuration % 60)}`
+    })
   })
 
   // stop and play sound
@@ -32,6 +41,18 @@ const app = () => {
       video.pause()
       play.src = './../svg/play.svg'
     }
+  }
+
+  // animated the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime
+    let elapsed = fakeDuration - currentTime
+    let seconds = Math.floor(elapsed % 60)
+    let minutes = Math.floor(elapsed / 60)
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength
+    outline.style.strokeDashoffset = progress
+    // animate text
+    timeDisplay.textContent = `${minutes}:${seconds}`
   }
 }
 

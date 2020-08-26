d40602be73eb2af1b43a4178edbffbd441ae914a
Ana <ana.eng.dev@gmail.com>
2020-08-26 10:34:15 -0300

    webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  build: path.resolve(__dirname, './dist')
+}
+
+module.exports = {
+  entry: {
+    bundle: `${PATHS.src}/app.js`,
+    app: `${PATHS.src}/app.js`
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: PATHS.build
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: PATHS.build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: { loader: 'url-loader' }
+      }
+    ]
+  },
+  plugins: [
+    //new webpack.optimize.UglifyJsPlugin()
+    new HtmlWebpackPlugin({
+      title: 'TW Webpack',
+      template: `${PATHS.src}/index.html`
+    })
+  ]
+}

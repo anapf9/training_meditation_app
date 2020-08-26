1379fa58c5b08b217cfd34a2d3f9f84288d73ee9
Ana <ana.eng.dev@gmail.com>
2020-08-26 12:33:53 -0300

    song video and svg play and pause

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const app = () => {
+  const song = document.querySelector('.song')
+  const play = document.querySelector('.play')
+  const outline = document.querySelector('.moving-outline circle')
+  const video = document.querySelector('.vid-container video')
+
+  //sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+  //time display
+  const timeDisplay = document.querySelector('.time-display')
+  //get delete de outline
+  const outlineLength = outline.getTotalLength()
+  // duration
+  let fakeDuration = 600
+  outline.style.strokeDasharray = outlineLength
+  outline.style.strokeDashoffset = outlineLength
+
+  // play sound
+  play.addEventListener('click', () => {
+    checkPlaying(song)
+  })
+
+  // stop and play sound
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play()
+      video.play()
+      play.src = './../svg/pause.svg'
+    }
+    else {
+      song.pause()
+      video.pause()
+      play.src = './../svg/play.svg'
+    }
+  }
+}
+
+app()
